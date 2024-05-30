578af4391ba65a82611d9e1a97ac4e62bf48c365
IRTIDEATH <enderjhass@gmail.com>
2024-05-30 19:48:30 +0700

    upd: add link in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Home = () => {
   return (
-    <div>Homes</div>
+    <div className="w-full h-screen flex flex-col items-center justify-center space-y-8">
+      <h1 className="text-2xl font-semibold">Home Page</h1>
+      <div className="flex items-center space-x-2">
+        <p>Go to Product page :</p>
+        <Link className="py-2 px-3 bg-[#fff] rounded-md text-sm font-medium text-[#4b4b4b]" href={'/products'}>Products</Link>
+      </div>
+    </div>
   )
 }
 
